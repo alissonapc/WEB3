d3480b7850f3cee4cb210ad787d5fc83d75b0c2f
THOMAS BATISTA PINTO <CJ3035034@ifspcjoad.ifspcjo.edu.br>
2026-08-06 20:35:33 -0300

    feat: new table

--- ifitness-ui/src/app/app.component.ts
+++ ifitness-ui/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    TableModule,
+    TooltipModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ifitness-ui';
+
+  activities = [
+    { type: 'CORRIDA', date: '29/07/2026', distance: 8.0, duration: 42, user: 'Fernando Duarte' },
+    { type: 'CORRIDA', date: '30/07/2026', distance: 8.0, duration: 43, user: 'Fernando Duarte' },
+    { type: 'CAMINHADA', date: '30/07/2026', distance: 5.0, duration: 55, user: 'Juliana Silva' }
+  ];
+
 }
